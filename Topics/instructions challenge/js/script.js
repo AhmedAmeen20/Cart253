e236629c6a59759807f678fb4b4b36e38e8781c5
AhmedAmeen20 <Ah_ameen@live.concordia.ca>
2026-09-15 16:12:21 -0400

    tattoine half done

--- Topics/instructions challenge/js/script.js
+++ Topics/instructions challenge/js/script.js
@@ -1,24 +1,34 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
-
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
+    createCanvas(600, 400);
 
-}
+    rectMode(CENTER)
+
+    background(84, 33, 114);
+
+    fill(50, 255, 50)
+    noStroke();
+    rect(305, 600, 600, 400, 600);
+
+    fill(119, 88, 0)
+    rect(300, 400, 600, 200)
+
+    fill(240, 200, 150)
+    circle(175, 200, 250);
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
 
+    quad(50, 210, 400, 160, 400, 360, 50, 360);
+
+
+    fill(255, 190, 174)
+    circle(400, 100, 70)
+
+    fill(255, 226, 219)
+    circle(500, 200, 70)
+
+
+
+
+
+    describe('A white square with a black outline on a gray canvas.');
 }
